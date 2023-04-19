76f6d0774e1a7adb748a5cc5f2ae7cc29fcfb8f9
Ted - TedfordMedia <ted@tedfordmedia.com>
2023-04-19 17:42:50 +0100

    x

--- src/RoutesTree.tsx
+++ src/RoutesTree.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import MainPage from "./pages/MainPage";
+import Brain from "./pages/Brain";
 
 const RoutesTree: React.FC = () => {
     return (
         <Routes>
             <Route path="/" element={<MainPage />} />
+            <Route path="/Brain" element={<Brain />} />
         </Routes>
     );
 };
